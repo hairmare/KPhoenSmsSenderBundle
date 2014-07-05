10af31a99cce573178b93806898ddafc7e60e42a
Lucas Bickel <hairmare@purplehaze.ch>
2014-07-05 13:11:41 +0200

    basic integration for swisscom-provider

---
+++ DependencyInjection/Factory/SwisscomProviderFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace KPhoen\SmsSenderBUndle\DependencyInjection\Factory;
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * swisscom provider factory
+ *
+ * @author Lucas Bickel <hairmare@purplehaze.ch>
+ */
+class SwisscomProviderFactory implements ProviderFactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+        $container->getDefinition($id)
+            ->replaceArguments(1, $config['api_key'])
+            ->replaceArguments(2, $config['international_prefix'])
+        ;
+    }
+
+    /**
+     * {@inheriDoc}
+     */
+    public function getKey()
+    {
+        return 'swisscom';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addConfiguration(NodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->scalarNode('api_key')->isRequired()->end()
+                ->sdcalarNode('international_prefix')->defaultValue('+41')->end()
+            ->end()
+        ;
+    }
+}
